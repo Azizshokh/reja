e534829d3e94d8aeec7628908855369c5c7267b8
ALI <erali.jurakulov@mail.ru>
2025-12-23 02:27:31 +0900

    111

--- server.js
+++ server.js
@@ -17,14 +17,32 @@ app.set("view engine", "ejs"); //BSSR Backenda Frontend qurish
 
 // 4 Routing codes: Routerlar un
 // serverga path qilish un:
+<<<<<<< HEAD
 app.get("/hello", function(req, res){
     res.end(`<h1 style="background: yellow">Hello World by ALI!!!</h1>`);
     // res.end - response javob tugashi
     // res.send - response jo'natish va automatik tugatish
+=======
+
+
+// app.get("/hello", function(req, res){
+//     res.end(`<h1 style="background: yellow">Hello World by ALI!!!</h1>`);
+//     // res.end - response javob tugashi
+//     // res.send - response jo'natish va automatik tigatish
+// });
+
+// app.get("/gift", function(req, res){
+//     res.send(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res)=>{
+    console.log(req.body);
+    res.json({test: "success"});
+>>>>>>> 4c9f7aa (Last version)
 });
 
-app.get("/gift", function(req, res){
-    res.send(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+app.get("/", function(req, res){
+    res.render("harid")
 });
 
 const server = http.createServer(app);
@@ -33,5 +51,24 @@ server.listen(PORT, function () {
     console.log(`The server is running succesfully on port: ${PORT}`);    
 });
 
+<<<<<<< HEAD
 
+=======
+/*
+1) git init -- qilib (.gitignore) file ocamiz
+2) node_modules
+3) git status
+4) git add .
+5) git status
+6) git commit -m "BRR: building express web server"
+   (git commit --ammend) - oxirgi source birlashtirb yoz degandi, remote ga solib push qilingan bo'lsa bu commit shartmas 
+7) git log --oneline
+8) git branch
+*) (git add server.js; git commit -m "Fix server.js configuration"; git status)
+9) git remote - remote ni tekshirish
+10) git branch -M master
+11) git push origin master
+12) git log --oneline (local mashina remote masterni ko'rish un)
+*/ 
+>>>>>>> 4c9f7aa (Last version)
  
